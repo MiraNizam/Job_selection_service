79af1fdbd099049d4cc16b596cf2d5d1caada5a9
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-06-19 15:02:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import requests
+from itertools import count
+
+
+def get_hh_vacancies(url: str, languages: list) -> tuple:
+    for language in languages:
+        payload = {
+            "text": f"программист {language}",
+            "area": "1",
+            "period": 30,
+            "only_with_salary": True,
+            "currency": "RUR"
+        }
+        response = requests.get(f"{url}/vacancies", params=payload)
+        response.raise_for_status()
+        vacancies = response.json()
+        yield language, vacancies
+
+
+def get_hh_statistics(language: str, vacancies: dict) -> dict:
+    summary_statistics = {}
+    sum_salaries = 0
+    vacancies_processed = 0
+
+    for vacancy in vacancies["items"]:
+        expected_salary = predict_rub_salary(vacancy["salary"])
+        sum_salaries += expected_salary
+        vacancies_processed += 1
+    average_salary = sum_salaries / vacancies_processed
+
+    language_statistics = {
+        "vacancies_found": vacancies["found"],
+        "vacancies_processed": vacancies_processed,
+        "average_salary": average_salary
+    }
+    summary_statistics[language] = language_statistics
+    return summary_statistics
+
+
+def predict_rub_salary(vacancy: dict) -> int:
+    if vacancy['from'] is not None and vacancy['to'] is not None:
+        return int((vacancy['from'] + vacancy['to']) / 2)
+    elif vacancy['from'] is not None and vacancy['to'] is None:
+        return int(vacancy['from']*1.2)
+    elif vacancy['from'] is None and vacancy['to'] is not None:
+        return int(vacancy['to'] * 0.8)
+
+
+if __name__ == '__main__':
+    url = "https://api.hh.ru"
+    languages = ["JavaScript", "Java", "Python", "Ruby", "PHP", "C++", "C#", "C", "Go", "TypeScript"]
+
+    for language, vacancies in get_hh_vacancies(url, languages):
+        print(get_hh_statistics(language, vacancies))
+
+    #      пагинация
+    # for page in count():
+    #     payload = {
+    #         "text": f"программист {language}",
+    #         "area": "1",
+    #         "period": 30,
+    #         "only_with_salary": True,
+    #         "currency": "RUR",
+    #         "per_page": 10,
+    #         "page": page
+    #     }
+    #     response = requests.get(f"{url}/vacancies", params=payload)
+    #     response.raise_for_status()
+    #     vacancies = response.json()
+    #     url_pages.append(vacancies)
+    #
+    #     if page >= vacancies["found"] / 10 or page >= 100:
+    #         break
