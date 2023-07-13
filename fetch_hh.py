6b1c33500760cff49d231f140d07c67d15f0918c
Эльмира Низамова <e.nizamova@integra.local>
2023-07-13 16:14:06 +0300

    Update fetch_hh.py

--- fetch_hh.py
+++ fetch_hh.py
@@ -4,11 +4,13 @@ from collections import Counter
 
 
 def get_hh_vacancies(url: str, languages: list) -> tuple:
+    full_information = {}
     for language in languages:
+        list_vacancies = []
         for page in count(1):
             payload = {
                 "text": f"программист {language}",
-                "area": "1",
+                "area": "1", # Москва
                 "period": 30,
                 "only_with_salary": True,
                 "currency": "RUR",
@@ -18,42 +20,39 @@ def get_hh_vacancies(url: str, languages: list) -> tuple:
             response = requests.get(f"{url}/vacancies", params=payload)
             response.raise_for_status()
             vacancies = response.json()
-            if page >= vacancies["found"] / 100 or page >= 20:
+            list_vacancies.append(vacancies)
+            full_information[language] = list_vacancies
+            if page > int(vacancies["found"] / 100) or page >= 20:
                 break
-            yield language, vacancies
+    return full_information
 
 
-def get_hh_page_statistics(language: str, vacancies: dict) -> dict:
-    sum_salaries = 0
-    vacancies_processed = 0
+def get_hh_statistics(full_vacancies: dict) -> dict:
+    total_statistics = {}
+    for language, vacancies in full_vacancies.items():
+        sum_salaries = 0
+        vacancies_processed = 0
+        try:
+            average_salary = int(sum_salaries / vacancies_processed)
+        except ZeroDivisionError as e:
+            average_salary = 0
 
-    for vacancy in vacancies["items"]:
-        expected_salary = predict_rub_salary_hh(vacancy["salary"])
-        sum_salaries += expected_salary
-        vacancies_processed += 1
-
-    language_statistics_page = {
-        "language": language,
-        "vacancies_found": vacancies["found"],
-        "vacancies_processed": vacancies_processed,
-        "sum_salaries": sum_salaries
-    }
-    return language_statistics_page
-
-        # try:
-        #     average_salary = int(sum_salaries / vacancies_processed)
-        # except ZeroDivisionError as e:
-        #     average_salary = 0
-        #
-        # summary_statistics = {}
-        # language_statistics = {
-        #     "vacancies_found": vacancies["found"],
-        #     "vacancies_processed": vacancies_processed,
-        #     "average_salary": average_salary
-        # }
-        #
-        # summary_statistics[language] = language_statistics
-        # return summary_statistics
+        for page_vacancies in vacancies:
+            for vacancy in page_vacancies["items"]:
+                expected_salary = predict_rub_salary_hh(vacancy["salary"])
+                sum_salaries += expected_salary
+                vacancies_processed += 1
+                try:
+                    average_salary = int(sum_salaries / vacancies_processed)
+                except ZeroDivisionError as e:
+                    average_salary = 0
+            language_statistics = {
+                "vacancies_found": page_vacancies["found"],
+                "vacancies_processed": vacancies_processed,
+                "average_salary": average_salary
+            }
+            total_statistics[language] = language_statistics
+    return total_statistics
 
 
 def predict_rub_salary_hh(vacancy: dict) -> int:
@@ -63,14 +62,13 @@ def predict_rub_salary_hh(vacancy: dict) -> int:
         return int(vacancy["from"]*1.2)
     elif vacancy["from"] is None and vacancy["to"] is not None:
         return int(vacancy["to"] * 0.8)
-
+#
 
 if __name__ == '__main__':
     url = "https://api.hh.ru"
     languages = ["JavaScript", "Java", "Python", "Ruby", "PHP", "C++", "C#", "C", "Go", "TypeScript"]
     get_hh_vacancies = get_hh_vacancies(url, languages)
+    hh_stat = get_hh_statistics(get_hh_vacancies)
+    print(hh_stat)
 
-    common_statistics = [get_hh_page_statistics(language, vacancies) for language, vacancies in
-                         get_hh_vacancies]
-    print(common_statistics)
 
