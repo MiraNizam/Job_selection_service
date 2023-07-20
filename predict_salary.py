9ddbbe4867b43db98dc05861e432afad939ee6b3
Эльмира Низамова <e.nizamova@integra.local>
2023-07-20 16:11:19 +0300

    Update predict_salary.py

--- predict_salary.py
+++ predict_salary.py
@@ -1,9 +1,7 @@
-def predict_salary(vacancy: dict or None, payment_from: str, payment_to: str, currency: str) -> int or None:
-    if not vacancy:
-        return None
-    elif vacancy[payment_from] and vacancy[payment_to] and vacancy["currency"] == currency:
-        return int((vacancy[payment_from] + vacancy[payment_to]) / 2)
-    elif vacancy[payment_from] and not vacancy[payment_to] and vacancy["currency"] == currency:
-        return int(vacancy[payment_from]*1.2)
-    elif not vacancy[payment_from] and vacancy[payment_to] and vacancy["currency"] == currency:
-        return int(vacancy[payment_to] * 0.8)
+def predict_salary(from_salary: int or None, to_salary: int or None) -> int:
+    if from_salary and to_salary:
+        return int((from_salary + to_salary) / 2)
+    elif from_salary and not to_salary:
+        return int(from_salary*1.2)
+    elif not from_salary and to_salary:
+        return int(to_salary * 0.8)
