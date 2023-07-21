dc02433fde85ca952a30ff8ff717e75d643de2ea
Эльмира Низамова <e.nizamova@integra.local>
2023-07-21 17:58:41 +0300

    Refactor rename variable

--- statistics_table.py
+++ statistics_table.py
@@ -23,7 +23,7 @@ def create_table(statistics: dict, title: str):
 
 def main():
     load_dotenv()
-    secret_key = os.getenv("SECRET_KEY")
+    secret_key = os.getenv("SJ_SECRET_KEY")
     languages = ["JavaScript","Java","Python","PHP","C++","C#","C"]
     hh_title = "HeadHunter Moscow"
     sj_title = "SuperJob Moscow"
